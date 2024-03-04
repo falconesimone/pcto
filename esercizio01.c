64f51a5693af95266b24cdd72eded75d00a36240
falconesimone <162116808+falconesimone@users.noreply.github.com>
2024-03-04 16:01:40 +0000

    sf

---
+++ esercizio01.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() 
+{
+int numero;
+numero=20;
+printf("%d",numero);
+}
---
+++ esercizio02.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main ()
+{
+char lettera;
+lettera= 'b';
+printf("%d",lettera);
+}
