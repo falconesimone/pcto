576ced2cb975cd2209ec8a7769f23bd858013690
falconesimone <162116808+falconesimone@users.noreply.github.com>
2024-03-05 15:54:33 +0000

    a

---
+++ esercizio05.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+ int numero; 
+ printf("inserisci un numero:"); 
+ scanf ("%d", &numero);
+ if ( numero % 2==0) { 
+  printf("il numero é pari\n"); 
+ } else 
+  printf("il numero é dispari\n"); 
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ esercizio06.c
@@ -0,0 +1,29 @@
+#include <stdio.h> 
+
+int main()
+{
+
+ int num1, num2, num3; 
+ printf("inserisci un numero: "); 
+ scanf("%d\n", &num1); 
+ printf("inserisci un secondo numero:");
+ scanf("%d\n", &num2);  
+ printf("inserisci un terzo numero:"); 
+ scanf("%d\n", &num3);
+ int differenza_1= num2-num1; 
+ int differenza_2= num3-num2;
+
+  if (differenza_1== differenza_2);
+  {
+ printf("\n i numeri sono in pogressione aritmetica"); 
+ else if 
+  printf("\n i numeri non sono in progressione aritmetica");
+  }
+
+
+
+
+
+
+
+}
